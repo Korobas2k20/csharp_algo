2e6550de018487b0ea9550e93f2bfef3d5862c80
Korobas2k20 <62026527+Korobas2k20@users.noreply.github.com>
2025-03-30 00:42:23 +0500

    Create Crickets.cs

---
+++ Crickets.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Этот метод вызывается каждый кадр
+    void Update()
+    {
+        // Выводим слово "crickets" в консоль каждый кадр
+        Debug.Log("crickets");
+    }
+}
